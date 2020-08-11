5e1722d2da65eb08aeab6b9d55417f8bf84aa567
Tasfik Rahman <tasfikrahman001@gmail.com>
2020-08-11 18:26:57 +0600

    user individual profile route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,29 @@
 // Connect to MongoDB through mongoose
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 /* 
-@route      |       GET api/profile
-@desc       |       test route
-@access     |       public
+@route      |       GET api/profile/me
+@desc       |       Get current user profile
+@access     |       Private
 */
-router.get('/', (req, res) => res.send('Profile route'));
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error!!');
+    }
+});
 
 // exporting the route to server.js file
 module.exports = router;
